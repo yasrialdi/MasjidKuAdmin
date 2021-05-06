9081ed605a165965b97e024ae89497c0f9210cb4
yasrialdi <72615276+yasrialdi@users.noreply.github.com>
2021-05-06 23:06:34 +0700

    Layout Profil Masjid, Tampilan belum sempurna

---
+++ app/src/main/java/com/masjidtrpl/masjidku_admin/ProfilMasjidActivity.java
@@ -0,0 +1,14 @@
+package com.masjidtrpl.masjidku_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilMasjidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_masjid);
+    }
+}
