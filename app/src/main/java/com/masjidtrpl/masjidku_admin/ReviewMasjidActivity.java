f7287a9290e4db37b9a4c4e9ebb0a61efb8c2b34
yasrialdi <72615276+yasrialdi@users.noreply.github.com>
2021-05-31 14:14:23 +0700

    Layout main masjid dan activity review masjid

---
+++ app/src/main/java/com/masjidtrpl/masjidku_admin/ReviewMasjidActivity.java
@@ -0,0 +1,14 @@
+package com.masjidtrpl.masjidku_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReviewMasjidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_masjid);
+    }
+}
