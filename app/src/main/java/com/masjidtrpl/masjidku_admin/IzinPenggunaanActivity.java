36533060ecd60bffc244012cca5dbfc52511d958
M. Ikhsan Gumanof <ikhsangumanof@gmail.com>
2021-06-30 13:38:45 +0700

    Update IzinPenggunaanActivity.java

--- app/src/main/java/com/masjidtrpl/masjidku_admin/IzinPenggunaanActivity.java
+++ app/src/main/java/com/masjidtrpl/masjidku_admin/IzinPenggunaanActivity.java
@@ -2,6 +2,7 @@ package com.masjidtrpl.masjidku_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class IzinPenggunaanActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class IzinPenggunaanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_izin_penggunaan);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(IzinPenggunaanActivity.this, MainMasjidActivity.class));
+        finish();
     }
 }
