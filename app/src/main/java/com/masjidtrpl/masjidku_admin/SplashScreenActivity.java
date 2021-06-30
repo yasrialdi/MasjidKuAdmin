27a12ff18cfab8efdca95f7323c66237c0139bb7
M. Ikhsan Gumanof <ikhsangumanof@gmail.com>
2021-06-30 14:22:04 +0700

    Update SplashScreenActivity.java

--- app/src/main/java/com/masjidtrpl/masjidku_admin/SplashScreenActivity.java
+++ app/src/main/java/com/masjidtrpl/masjidku_admin/SplashScreenActivity.java
@@ -2,10 +2,14 @@ package com.masjidtrpl.masjidku_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.widget.Toast;
 
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -16,12 +20,35 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
-                finish();
-            }
-        }, 3000);
+
+        if (checkInternet()){
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                    finish();
+                }
+            }, 3000);
+        } else{
+            Toast.makeText(this, "Aplikasi anda tidak terhubung dengan internet", Toast.LENGTH_SHORT).show();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    overridePendingTransition(0,0);
+                    finish();
+                    overridePendingTransition(0, 0);
+                    startActivity(getIntent());
+                    overridePendingTransition(0, 0);
+                }
+            }, 5000);
+        }
+    }
+
+    public boolean checkInternet(){
+        boolean connectStatus = true;
+        ConnectivityManager ConnectionManager=(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo=ConnectionManager.getActiveNetworkInfo();
+        connectStatus = networkInfo != null && networkInfo.isConnected();
+        return connectStatus;
     }
 }
